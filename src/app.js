b73dee78655e8c3252ed1285bb2a38aaff99a7d6
federicapoletti24 <federicapoletti24@users.noreply.github.com>
2021-01-30 15:59:02 +0100

    Insert Date and time JS

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+function showDate(timestamp) {
+let date = new Date(timestamp);
+let hours = date.getHours(timestamp);
+if (hours <10) {
+    hours = `0${hours}`;
+}
+
+let minutes = date.getMinutes(timestamp);
+if (minutes <10) {
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay(timestamp)];
+
+return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
     console.log(response.data);
     let temperature = Math.round(response.data.main.temp);
@@ -15,9 +32,10 @@ function displayTemperature(response) {
     let windElement = document.querySelector("#wind");
     let windValue = response.data.wind.speed;
     windElement.innerHTML = Math.round(windValue);
+    let date = document.querySelector("#date");
+    date.innerHTML = showDate(response.data.dt * 1000);
     
 }
-
 
 let apiKey = "dc8f5bf2676eeecb4b285e5dcb7dcb71";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
